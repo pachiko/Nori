6adae4078fd796141ae6bb758f7f6a7b30e38741
pachiko <phillippang1994@gmail.com>
2022-12-30 15:22:23 +0800

    Whitted Ray Tracer w/ Dielectric

--- src/dielectric.cpp
+++ src/dielectric.cpp
@@ -31,7 +31,54 @@ public:
     }
 
     Color3f sample(BSDFQueryRecord &bRec, const Point2f &sample) const {
-        throw NoriException("Unimplemented!");
+        bRec.measure = EDiscrete;
+
+        float cosThetaI = Frame::cosTheta(bRec.wi);
+        float Fr = fresnel(cosThetaI, m_extIOR, m_intIOR);
+
+        if (sample.x() < Fr) {
+            // reflect
+            bRec.eta = 1.f;
+
+            bRec.wo = Vector3f(
+                -bRec.wi.x(),
+                -bRec.wi.y(),
+                cosThetaI
+            );
+
+            return Color3f(1.f);
+        } else { 
+            // refract
+            float etaI = m_extIOR, etaT = m_intIOR;
+
+            /* Swap the indices of refraction if the interaction starts
+            at the inside of the object */
+            if (cosThetaI < 0.0f) {
+                std::swap(etaI, etaT);
+                // No need to negate cosThetaI since we square it here.
+            }
+
+            /* Using Snell's law, calculate the squared sine of the
+            angle between the normal and the transmitted ray */
+            float eta = etaI / etaT,
+                sinThetaTSqr = eta * eta * (1 - cosThetaI * cosThetaI);
+            float cosThetaT = std::sqrt(1.0f - sinThetaTSqr);
+
+            // what goes up, goes down; vice-versa.
+            if (cosThetaI > 0.f) cosThetaT = -cosThetaT;
+
+            bRec.eta = eta;
+
+            // vector component parallel to normal is cos, so perpendicular is sin
+            // use Snell's Law: eta_i sin_i = eta_t sin_t
+            bRec.wo = Vector3f(
+                -bRec.wi.x()*eta,
+                -bRec.wi.y()*eta,
+                cosThetaT
+            ).normalized();
+
+            return eta*eta*Color3f(1.f);
+        }
     }
 
     std::string toString() const {
--- src/whitted.cpp
+++ src/whitted.cpp
@@ -18,44 +18,55 @@ public:
         Intersection first;
         if (!scene->rayIntersect(ray, first)) return Color3f(0.f); // No hit
 
-        // Hit a light?
-        Color3f Le(0.f);
-        if (first.mesh->isEmitter()) {
+        const BSDF* f = first.mesh->getBSDF();
+
+        if (f->isDiffuse()) {
+            // Hit a light?
+            Color3f Le(0.f);
+            if (first.mesh->isEmitter()) {
+                EmitterQueryRecord rec;
+                rec.origin = ray.o;
+                rec.hit = first.p;
+                rec.hitN = first.shFrame.n;
+                rec.light = first.mesh;
+                Le = first.mesh->getEmitter()->eval(rec);
+            }
+
+            // Uniform-randomly pick a light.
+            const std::vector<Mesh*>& lights = scene->getEmitters();
+            const Mesh* light = lights[sampler->nextUInt(lights.size())];
+
+            // Sample the light
             EmitterQueryRecord rec;
-            rec.origin = ray.o;
-            rec.hit = first.p;
-            rec.hitN = first.shFrame.n;
-            rec.light = first.mesh;
-            Le = first.mesh->getEmitter()->eval(rec);
+            rec.origin = first.p;
+            rec.origN = first.shFrame.n;
+            rec.light = light;
+            Color3f Lr = light->getEmitter()->sample(sampler->next2D(), rec);
+            if (Lr.getLuminance() <= 0.f) return Le;
+            Lr /= (1.f / lights.size()); // Prob of choosing light
+
+            // Visibility
+            Vector3f v = rec.hit - rec.origin;
+            float d = v.norm();
+            v /= d;
+            Ray3f shadow(rec.origin, v, Epsilon, d - Epsilon);
+            if (scene->rayIntersect(shadow)) return Le;
+
+            // BSDF
+            BSDFQueryRecord bRec(first.shFrame.toLocal(-ray.d), first.shFrame.toLocal(v), ESolidAngle);
+            Color3f F = f->eval(bRec);
+            Lr *= F;
+
+            return Le + Lr;
+        } else if (sampler->next1D() < 0.95f) {
+            BSDFQueryRecord bRec(first.shFrame.toLocal(-ray.d));
+            Color3f res = f->sample(bRec, sampler->next2D());
+
+            Ray3f next(first.p, first.shFrame.toWorld(bRec.wo));
+            return res*Li(scene, sampler, next)/0.95f;
         }
 
-        // Uniform-randomly pick a light.
-        const std::vector<Mesh*>& lights = scene->getEmitters();
-        const Mesh* light = lights[sampler->nextUInt(lights.size())];
-
-        // Sample the light
-        EmitterQueryRecord rec;
-        rec.origin = first.p;
-        rec.origN = first.shFrame.n;
-        rec.light = light;
-        Color3f Lr = light->getEmitter()->sample(sampler->next2D(), rec);
-        if (Lr.getLuminance() <= 0.f) return Le;
-        Lr /= (1.f / lights.size()); // Prob of choosing light
-
-        // Visibility
-        Vector3f v = rec.hit - rec.origin;
-        float d = v.norm();
-        v /= d;
-        Ray3f shadow(rec.origin, v, Epsilon, d - Epsilon);
-        if (scene->rayIntersect(shadow)) return Le;
-
-        // BSDF
-        BSDFQueryRecord bRec(first.shFrame.toLocal(-ray.d), first.shFrame.toLocal(v), ESolidAngle);
-        const BSDF* f = first.mesh->getBSDF();
-        Color3f F = f->eval(bRec);
-        Lr *= F;
-
-        return Le + Lr;
+        return Color3f(0.f);
     }
 
     /// Return a human-readable description for debugging purposes
